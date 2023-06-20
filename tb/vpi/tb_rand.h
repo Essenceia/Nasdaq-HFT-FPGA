eb913ac1b33a101c5742088fd0d3fe19c14ed535
Julia Desmazes <julia.desmazes@gmail.com>
2023-06-20 15:29:35 -0700

    TB: Don't send a packet cnt of 0, not supported yet

--- tb/vpi/tb_rand.h
+++ tb/vpi/tb_rand.h
@@ -9,6 +9,7 @@ static inline void tb_rand_init(unsigned seed){
 }
 
 static inline uint16_t tb_rand_get_msg_cnt(){
-	return (uint16_t) rand() % MOLDUDP64_MSG_CNT_MAX;
+	uint16_t rand_cnt = (uint16_t) ( rand() % MOLDUDP64_MSG_CNT_MAX );
+	return rand_cnt ? rand_cnt : 1;
 }
 #endif//TB_RAND_H
